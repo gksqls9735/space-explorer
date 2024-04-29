9a56a9eb0895924d076f3a30e7475b336168e2c1
gksqls9735 <gksqls9735@gmail.com>
2024-04-29 22:41:47 +0900

    [add, update] picture add, carousel update

--- sitee/site.js
+++ sitee/site.js
@@ -1,3 +1,89 @@
+function call_js(){
+  //사진 및 앵커 참조변수 선언
+  let slideshow = document.querySelector(".slideshow");
+  let slideshow_sildes = document.querySelector(".slideshow_slides");
+  let slideshow_slides_a = document.querySelectorAll(".slideshow_slides a");
+
+  //화살표 객체 참조 변수 선언
+  let prev = document.querySelector(".prev");
+  let next = document.querySelector(".next");
+
+  //인디케이터 객체 참조 변수 선언
+  let indicators = document.querySelectorAll(".indicator a")
+  
+  //회전목마 현재 위치 값
+  let currentIndex = 0;
+  let timer = null;
+  let slideCount = slideshow_slides_a.length;
+  
+  //이미지 우축 배치
+  for(let i = 0;  i < slideCount; i++){
+    let newLeft = `${i*100}%`
+    slideshow_slides_a[i].style.left = newLeft;
+  }
+  //회전목마 움직이기 slideshow_slides 왼쪽으로 이동
+  function gotoSlide(index){
+    currentIndex = index;
+    let newLeft = `${index*-100}%`;
+    slideshow_sildes.style.left = newLeft;
+
+    //인디케이터 초기화 active를 지워주고 현재 인디케이터에 active 추가
+    indicators.forEach(e=>{
+      e.classList.remove("active")
+    })
+    indicators[currentIndex].classList.add("active")
+  }
+  //0~사진개수 -1까지 3초 딜레이 시키며 gotoSlide 호출
+  gotoSlide(0);
+  function startTime(){
+    timer = setInterval(()=>{
+      let index = (currentIndex+1)%slideCount;
+      gotoSlide(index)
+    }, 5000);
+  }
+  startTime();
+  //이벤트 처리 마우스 올리면 멈추기 내리면 다시 회전 시작
+  slideshow_sildes.addEventListener("mouseenter", ()=>{
+    clearInterval(timer);
+  })
+  slideshow_sildes.addEventListener("mouseleave",()=>{
+    startTime();
+  })
+  
+  //화살표 이벤트 처리
+  prev.addEventListener("click", e=>{
+    //a의 기능을 막아줌
+    e.preventDefault();
+    currentIndex -= 1;
+    //currentIndex가 0보다 작아지면 제일 마지막 사진부터 시작
+    if (currentIndex < 0) {
+      currentIndex = slideCount - 1;
+    }
+    gotoSlide(currentIndex);
+  })
+  next.addEventListener("click", e=>{
+    //a기능 막기
+    e.preventDefault();
+    currentIndex += 1;
+    //currentIndex가 slide의 개수의 length와 같아지면 처음으로 사진으로 돌아가기
+    if (currentIndex > slideCount -1) {
+      currentIndex = 0;
+    }
+    gotoSlide(currentIndex);
+  })
+  indicators.forEach(e=>{
+    e.addEventListener("mouseenter", ()=>{
+      clearInterval(timer);
+    })
+  })
+  for(let i = 0; i < indicators.length; i++){
+    indicators[i].addEventListener("click", e=>{
+      e.preventDefault();
+      gotoSlide(i);
+    })
+  }
+}
+
 function idCheck(){
   const memberID = document.querySelector("#memberID");
   const memberIDInfo = document.querySelector("#memberIDInfo");
