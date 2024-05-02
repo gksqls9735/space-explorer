cb1ea6111679f5ad8112f0248856d7698b830c05
gksqls9735 <gksqls9735@gmail.com>
2024-05-02 09:05:09 +0900

    [delete modify] img delete, board modify

--- board/board.js
+++ board/board.js
@@ -1,15 +1,17 @@
-document.querySelector("#more").addEventListener("click", function(){
+document.querySelector("#more").addEventListener("click", ()=>{
   const bulletinList =document.querySelector("#bulletinList");
   let date = new Date();
 
-  for(let i = 0; i < 3; i++){
-    const cell = bulletinList.insertRow();
+  for(let i = 0; i < 5; i++){
+    const cell = bulletinList.insertRow(0);
     const cell1 = cell.insertCell(0);
     const cell2 = cell.insertCell(1);
     const cell3 = cell.insertCell(2);
+    const cell4 = cell.insertCell(3);
 
     cell1.innerHTML = bulletinList.rows.length;
     cell2.innerHTML = `익명의 게시글 ${bulletinList.rows.length}`
     cell3.innerHTML = date.toDateString();
+    cell4.innerHTML = parseInt(Math.random()*10000);
   }
 })
